4571e819ebac3f68a3d79a7f39ebd7fe83b59523
MrCoderMan1 <90284153+MrCoderMan1@users.noreply.github.com>
2024-02-05 16:45:35 -0500

    Update MainCalc.java

--- src/MainCalc.java
+++ src/MainCalc.java
@@ -5,6 +5,7 @@ class MainCalc{
         Scanner scanner = new Scanner(System.in);
         System.out.println("***************** CALCULATOR *******************");
 
+        final double PI = 3.141;
         String[] equations = new String[100];
         String equation;
         double answer = 0; 
@@ -14,6 +15,7 @@ class MainCalc{
         int doAgain;
         int index = -1;
         int eqHistory;
+        byte pref;
 
         do{
             index++;
@@ -23,31 +25,33 @@ class MainCalc{
             System.out.println("Type 4 for Divison");
             System.out.println("Type 5 for the Square of a number\n");
 
+            System.out.println("Type 6 to find area of a shape.\n");
+
+            System.out.println("Type 7 to find area / circumference / diameter of a circle.\n");
+
+
             System.out.print("Number: ");
         
-            byte pref = scanner.nextByte();
+            byte firstPref = scanner.nextByte();
             System.out.println();
 
-            if(pref!=1 && pref!=2 && pref!=3 && pref!=4 && pref!=5){
+            if(firstPref!=1 && firstPref!=2 && firstPref!=3 && firstPref!=4 && 
+            firstPref!=5 && firstPref!=6 && firstPref!=7){
                 System.out.println("Invalid Answer");
-                System.out.println("************************************************");
-                answer=0;
-                scanner.close();
-                return;
             }
     
-            else if(pref != 5){
-                System.out.print("Enter your first number: ");
+            else if(firstPref != 5 && firstPref!=6 && firstPref!=7){
+                System.out.println("Enter your first number: ");
                 num1 = scanner.nextDouble(); System.out.println();
                 System.out.print("Enter your second number: ");
                 num2 = scanner.nextDouble();
             }
-            else if(pref == 5){
+            else if(firstPref == 5){
                 System.out.print("Enter your number: ");
                 num1 = scanner.nextDouble();
             }
     
-            switch (pref) {
+            switch (firstPref) {
                 case 1:
                     //Addition
                     answer = num1 + num2;
@@ -74,16 +78,81 @@ class MainCalc{
                     break;
                 case 5:
                     //Square
-                    answer = num1 * num1;
-                    equation = num1 + " * " + num1 + " = " + answer;
+                    answer = Math.pow(num1, 2);
+                    equation = num1 + " ^ 2 = " + answer;
                     equations[index] = equation;
                     break;
+                case 6:
+                    System.out.println("Enter 1 for triangle or type 2 for square");
+                    pref = scanner.nextByte();
+
+                    //Area
+                    switch (pref) {
+                        case 1:
+                            System.out.println("Enter side 1: ");
+                            num1 = scanner.nextDouble();
+                            System.out.println("Enter side 2: ");
+                            num2 = scanner.nextDouble();
+                            
+                            answer = (num1*num2)/2;
+                            equation = num1 + " * " + num2 + " / 2 " + " = " + answer + 
+                            " For triangle area";
+                            equations[index] = equation;
+                            break;
+                        case 2:
+                            System.out.println("Enter side 1: ");
+                            num1 = scanner.nextDouble();
+                            
+                            answer = Math.pow(num1, 2);
+                            equation = num1 + " ^ 2 = " + answer + " For area of square";
+                            equations[index] = equation;
+                            break;
+                        
+                        default:
+                            System.out.println("Invalid number.");
+                            break;
+                    }
+                    case 7:
+                        //Circle circumference, diameter, and area
+                        System.out.println("Type 1 for area, 2 for circumference, and 3 for diameter");
+
+                        pref = scanner.nextByte();
+                        switch (pref) {
+                            case 1:
+                                System.out.println("Enter radius of your circle: ");
+                                num1 = scanner.nextDouble();
+
+                                answer = Math.pow(num1, 2) * PI;
+                                equation = num1 + " ^ 2 * PI(3.141) = " +  answer + " For area of circle";
+                                equations[index] = equation;
+                                break;
+                            case 2:
+                                System.out.println("Enter radius of your circle: ");
+                                num1 = scanner.nextDouble();
+
+                                answer = 2 * (PI * num1);
+                                equation = "(" + num1 + " * PI) * 2 = " + answer + " For the circumference";
+                                equations[index] = equation;
+                                break;
+                            case 3:
+                                System.out.println("Enter radius of your circle: ");
+                                num1 = scanner.nextDouble();
+
+                                answer = num1 * 2;
+                                equation = num1 + " * 2 = " + answer + " For the diameter";
+                                equations[index] = equation;
+                                break;
+                        
+                            default:
+                                System.out.println("Invalid number");
+                                break;
+                }
             }
     
             System.out.println("\nAnswer is: " + answer + "\n");
             
             System.out.println("Do you want to view your equation history?");
-            System.out.print("Press 1 for yes and 2 for no: ");
+            System.out.print("Type 1 for yes and 2 for no: ");
 
             eqHistory = scanner.nextInt();
 
